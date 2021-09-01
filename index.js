986bce38b4d4e7647574441127b30689bce221f5
Goldie <goldiejackson25@yahoo.com>
2021-09-01 12:58:12 -0500

    commit tree e1ba977f4d697b7c1cfbe531885721eb2d27e9c5

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+/*/ Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers[0][1]; - doesn't work because it doesn't create a new array???
+}
+*/
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return function(fare){
+        return fare * int;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnDrivers){
+    if (returnDrivers === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers);
+    }
+    else {return returnLastTwoDrivers(drivers)};
+}
